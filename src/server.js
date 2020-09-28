5d013733d9ef9acd6b438d6cbf29a9ae11860a62
Marcones <markonespro@gmail.com>
2020-09-28 02:23:42 +0200

    joinha
    con j

--- src/server.js
+++ src/server.js
@@ -1,85 +1,12 @@
-//Dados
-const proffys = [
-     {
-     name:"Diego Fernandes",
-     avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-     whatsapp:"89987654534",
-      bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-      subject:"Quimica", 
-      cost:"20",
-      weekday: [0], 
-      tim_from:[720], 
-      time_to:[1220]  
-     },
-      {
-         name:"Daniel Evngelista",
-         avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-         whatsapp:"89987654534",
-          bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-          subject:"Quimica", 
-          cost:"20",
-          weekday: [1], 
-          tim_from:[720], 
-          time_to:[1220]  
-      },
-      {
-         name:"Mark",
-         avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-         whatsapp:"89987654534",
-          bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-          subject:"Quimica", 
-          cost:"20",
-          weekday: [1], 
-          tim_from:[720], 
-          time_to:[1220]  
-      }
- ]   
- 
- const subjects = [    
-     "Artes",
-     "Biologia",
-     "Ciências",
-     "Educação física",
-     "Física",
-     "Geografia",
-     "História",
-     "Matemática",
-     "Português",
-     "Química",
- ] 
- 
- const weekdays = [
-     "Domingo",
-     "Domingo",
-     "Segunda-feira",
-     "Terça-feira",
-     "Quarta-feira",
-     "Quinta-feira",
-     "Sexta-feira",
-     "Sábado",
- ]
- 
- //Funcionalidades
- function pageLanding(req, res) {
-     return res.render("index.html")
- }
- 
- function pageStudy(req, res) {   
-     const filters = req.query
-     return res.render("study.html", {proffys, filters, subjects, weekdays})
- }
- 
- function pageGiveClasses(req, res) { 
-    
- //adicionar dados a lista de proffys
-  
- 
-     return res.render("give-classes.html", { subjects, weekdays})
- }
- 
-  //Servidor
+ //Servidor
   const express = require('express')
   const server = express()
+
+  const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses
+} = require('./pages')
   
   
   //configurar nunjucks (template engine)
